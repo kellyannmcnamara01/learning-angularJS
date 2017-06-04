cb411be09ea19867a57d730f9b65c71ba0d8a80e
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-06-04 16:56:39 -0400

    following the tut, create an object called Hero within the class AppComponent

--- elementary01/elementary/src/app/app.component.js
+++ elementary01/elementary/src/app/app.component.js
@@ -8,14 +8,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Kelly';
+        //hero = "Windstorm";
+        //make an object of hero
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- elementary01/elementary/src/app/app.component.ts
+++ elementary01/elementary/src/app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
 })
-export class AppComponent  { name = 'Angular'; }
+
+
+export class AppComponent  {
+  title = 'Kelly';
+  //hero = "Windstorm";
+  //make an object of hero
+  hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+  };
+}
+
