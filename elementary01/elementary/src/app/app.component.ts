f05b64cfee3ecf0063a59336a438f162e977ecc0
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-06-04 19:35:59 -0400

    create an array and have a ngFor populate an li element for each index in the array

--- elementary01/elementary/src/app/app.component.ts
+++ elementary01/elementary/src/app/app.component.ts
@@ -11,6 +11,13 @@ import { Component } from '@angular/core';
             <label>name: </label>
             <input [(ngModel)]="hero.name" placeholder="name">
         </div>
+        <div>
+            <ul class="heroes">
+              <li *ngFor="let hero of heroes">
+                <span>{{hero.id}}</span> {{hero.name}}
+              </li>
+            </ul>
+        </div>
         `,
 })
 
@@ -23,5 +30,19 @@ export class AppComponent  {
   id: 1,
   name: 'Windstorm'
   };
+  heroes = HEROES;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
