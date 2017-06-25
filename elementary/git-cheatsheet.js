20803aa4fdc8a5fe94e85bd5054887725e39f147
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-06-25 18:12:46 -0400

    tried to add one ng-model to two ng-repeats... it doesn't work, the search bar might not work

--- elementary/git-cheatsheet.js
+++ elementary/git-cheatsheet.js
@@ -1,14 +1,5 @@
 var app = angular.module('eleAppTwo', []);
 
-// trying to make a filter for unique section names
-/*app.filter('unique', function () {
- return function (item) {
- return function (item) {
- return item.toUpperCase();
- }; //end return function
- }); //enf app.filter*/
-
-
 
 app.controller('eleCtrlTwo', function($scope, $http) {
     //start of the http.get for the git sections
@@ -24,17 +15,18 @@ app.controller('eleCtrlTwo', function($scope, $http) {
         }); //end of http.get for the git sections
 });
 
-/*
- app.controller('eleCSS', function($scope, $http) {
- //start of the http.get for the git sections
- $http.get("http://localhost/5303/elementary/git-api/public/sections")
- .then(function(response) {
- var testing = response.data;
- var apple = [];
- for(var i = 0; i < testing.length; i++){
- apple.push(testing[i]);
- }
- $scope.apple = apple;
- $scope.prev = "";
- }); //end of http.get for the git sections
- });*/
+app.controller('eleCss', function($scope, $http) {
+    $http.get("http://localhost/5303/elementary/git-api/public/cssSections")
+        .then(function(response) {
+            var testing = response.data;
+            var cssSection = [];
+            for(var i = 0; i < testing.length; i++){
+                cssSection.push(testing[i]);
+            }
+            //console.log(apple[0].section);
+            $scope.cssSection = cssSection;
+            $scope.prev = "";
+            console.log($scope.cssSection);
+        });
+});
+
