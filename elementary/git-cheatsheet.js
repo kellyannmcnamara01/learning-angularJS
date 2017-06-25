c066ec04a0bd1e86517cd59e53bc49c3447ae28f
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-06-25 17:06:58 -0400

    set up the http in testing for css

--- elementary/git-cheatsheet.js
+++ elementary/git-cheatsheet.js
@@ -2,37 +2,39 @@ var app = angular.module('eleAppTwo', []);
 
 // trying to make a filter for unique section names
 /*app.filter('unique', function () {
-    return function (item) {
-    return function (item) {
-        return item.toUpperCase();
-    }; //end return function
-}); //enf app.filter*/
+ return function (item) {
+ return function (item) {
+ return item.toUpperCase();
+ }; //end return function
+ }); //enf app.filter*/
 
 
 
 app.controller('eleCtrlTwo', function($scope, $http) {
+    //start of the http.get for the git sections
     $http.get("http://localhost/5303/elementary/git-api/public/sections")
         .then(function(response) {
             var testing = response.data;
-            //$scope.section = testing[0].section;
-            //$scope.description = testing[0].description;
-            //$scope.codesnippet = testing[0].codesnippet;
             var apple = [];
             for(var i = 0; i < testing.length; i++){
-                //var section = testing[i].section;
-                //var description = testing[i].description;
-                //var codesnippet = testing[i].codesnippet;
                 apple.push(testing[i]);
-                //$scope.apple = testing[i].section;
-                //$scope.description = testing[i].description;
-                //$scope.codesnippet = testing[i].codesnippet;
-
-                //console.log("section:" + $scope.section + " ||||||| desc:" + $scope.description + " |||||||| code snipttet:" + $scope.codesnippet);
-                //console.log(apple);
             }
-            //console.log(apple[0].section);
             $scope.apple = apple;
             $scope.prev = "";
-            //console.log($scope.apple);
-        });
+        }); //end of http.get for the git sections
 });
+
+/*
+ app.controller('eleCSS', function($scope, $http) {
+ //start of the http.get for the git sections
+ $http.get("http://localhost/5303/elementary/git-api/public/sections")
+ .then(function(response) {
+ var testing = response.data;
+ var apple = [];
+ for(var i = 0; i < testing.length; i++){
+ apple.push(testing[i]);
+ }
+ $scope.apple = apple;
+ $scope.prev = "";
+ }); //end of http.get for the git sections
+ });*/
